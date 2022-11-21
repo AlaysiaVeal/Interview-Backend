e1c602d424055b3da40ee172fc591795768c8f14
Alaysia Veal <alaysiaveal@gmail.com>
2022-11-21 09:44:12 -0500

    migrating and fixing models

---
+++ migrations/20221121143506-create-student-course-grades.js
@@ -0,0 +1,46 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('student-course-grades', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      gradeId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'grades',
+          key: 'id'
+        }
+      },
+      studentId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'students',
+          key: 'id'
+        }
+      },
+      courseId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'courses',
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('student-course-grades')
+  }
+}
---
+++ models/student-course-grades.js
@@ -0,0 +1,46 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class StudentCourseGrades extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Student -
+    Course -
+    Grades.init(
+      {
+        gradeId: {
+          type: DataTypes.INTEGER,
+          references: {
+            model: 'grades',
+            key: 'id'
+          }
+        },
+        studentId: {
+          type: DataTypes.INTEGER,
+          references: {
+            model: 'students',
+            key: 'id'
+          }
+        },
+        courseId: {
+          type: DataTypes.INTEGER,
+          references: {
+            model: 'courses',
+            key: 'id'
+          }
+        }
+      },
+      {
+        sequelize,
+        modelName: 'student-course-grades'
+      }
+    )
+  return StudentCourseGrades
+}
